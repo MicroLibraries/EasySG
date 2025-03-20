12f658a14c2d0369e6c3ba8d104ecd436d58e47d
Levi <72276897+sajjadarashhh@users.noreply.github.com>
2025-03-21 02:34:14 +0330

    builder completed

---
+++ src/Abstracts/EasySG.Abstracts/Packages/GeneratorPackage.cs
@@ -0,0 +1,34 @@
+﻿using EasySG.Abstracts.Generators;
+using EasySG.Abstracts.Generators.Interfaces;
+using EasySG.Abstracts.Generators.State;
+using EasySG.Abstracts.Models.CSharp;
+using EasySG.Abstracts.Packages.Interfaces;
+
+namespace EasySG.Abstracts.Packages
+{
+    internal class GeneratorPackage : IGeneratorPackage
+    {
+        public GeneratorPackage(List<IEGenerator> easyGenerators, List<IGeneratorRule> rules)
+        {
+            _state = new();
+            this.easyGenerators = easyGenerators;
+            this.rules = rules;
+        }
+
+        private readonly GeneratorState _state = null!;
+        private readonly List<IEGenerator> easyGenerators = null!;
+        private readonly List<IGeneratorRule> rules = null!;
+        public List<IResult> GetResults() => _state.GetResult();
+        public void Generate(CSharpClass input, GeneratorState state)
+        {
+            foreach (var generator in easyGenerators)
+            {
+                generator.Generate(input, _state);
+            }
+        }
+        public bool NeedToGenerate(CSharpClass @class)
+        {
+            return rules.All(a => a.NeedToGenerate(@class));
+        }
+    }
+}
---
+++ src/Abstracts/EasySG.Abstracts/Packages/Interfaces/IGeneratorPackage.cs
@@ -0,0 +1,11 @@
+﻿using EasySG.Abstracts.Generators;
+using EasySG.Abstracts.Generators.Interfaces;
+using EasySG.Abstracts.Generators.State;
+
+namespace EasySG.Abstracts.Packages.Interfaces
+{
+    public interface IGeneratorPackage : IEasyGenerator, IGeneratorRule
+    {
+        List<IResult> GetResults();
+    }
+}
